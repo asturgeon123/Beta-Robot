639ef025a5b6631a032c3e60aff66082b2ea181e
asturgeon123 <32243510+asturgeon123@users.noreply.github.com>
2018-06-28 11:20:24 -0500

    Add files via upload

---
+++ Stepper.py
@@ -0,0 +1,18 @@
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_StepperMotor
+ 
+import time
+import atexit
+
+mh = Adafruit_MotorHAT(addr = 0x60)
+
+def turnOffMotors():
+        mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+        mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+        mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+        mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+
+
+myStepper = mh.getStepper(200, 1)
+myStepper.setSpeed(30)
+ 
+atexit.register(turnOffMotors)
