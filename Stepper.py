d80948aa11fca559ec583880ea00aafd0dc6db0f
asturgeon123 <32243510+asturgeon123@users.noreply.github.com>
2018-06-28 12:32:49 -0500

    Update Stepper.py

--- Stepper.py
+++ Stepper.py
@@ -3,16 +3,19 @@ from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_Step
 import time
 import atexit
 
-mh = Adafruit_MotorHAT(addr = 0x60)
+mh = Adafruit_MotorHAT()
 
 def turnOffMotors():
         mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
         mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
         mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
         mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
-
+atexit.register(turnOffMotors)
 
 myStepper = mh.getStepper(200, 1)
 myStepper.setSpeed(30)
  
-atexit.register(turnOffMotors)
+ while (True):
+    print("Single coil steps")
+    myStepper.step(100, Adafruit_MotorHAT.FORWARD,  Adafruit_MotorHAT.SINGLE)
+    myStepper.step(100, Adafruit_MotorHAT.BACKWARD, Adafruit_MotorHAT.SINGLE)
